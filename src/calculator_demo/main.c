0eb01ab0ce00b4a2ec04df22c55a3c15bd81a4e3
Andy Mender <andymenderunix@gmail.com>
2020-12-25 00:18:43 +0100

    Add sources

---
+++ src/calculator_demo/main.c
@@ -0,0 +1,11 @@
+
+#define NK_IMPLEMENTATION
+#include <stdio.h>
+#include <nuklear.h>
+
+#include "main.h"
+
+int main(const int argc, const char** argv)
+{
+    printf("Hello, I'm a calculator!");
+}
